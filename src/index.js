3508d3fc8828f0a8dce2d83495a136af87bb4d80
Stan <stasdre@gmail.com>
2020-01-29 18:30:26 +0200

    task 4

--- src/index.js
+++ src/index.js
@@ -59,7 +59,9 @@ function returnFnResult(fn) {
    console.log(f()); // выведет 12
    console.log(f()); // выведет 13
  */
-function returnCounter(number) {}
+function returnCounter(number = 0) {
+    return () => ++number;
+}
 
 /*
  Задание 5 *:
